0c8212cbe2d54d29a927cbbda2c4f1e3df453e56
Vitaliy Gromov <gromov.vitaliy03@gmail.com>
2023-09-19 20:18:48 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            clientPort: 5173,
+            host: 'localhost',
+            protocol: 'ws'
+        }, 
+        port: 5173,
+        watch: {
+            usePolling: true
+        }
+    },
+    plugins: [
+        vue(
+            {
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }
+        ),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
